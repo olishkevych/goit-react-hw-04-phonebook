d46a58ff960d263d52e04a6fa8b7148c79ed205e
olishkevych <olishkevych.natalia@gmail.com>
2023-07-08 23:36:22 +0200

    add hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,69 @@
+import { nanoid } from 'nanoid';
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
+import { Filter } from './Filter/Filter';
+import { useState, useEffect } from 'react';
+import styles from './App.module.css';
+
 export const App = () => {
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    const strigifiedContacts = localStorage.getItem('contacts');
+    const contacts = JSON.parse(strigifiedContacts) ?? [];
+    setContacts(contacts);
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts.length]);
+
+  const addContact = ({ name, number }) => {
+    const isExisting = contacts.find(
+      contact => contact.name.toLowerCase() === name.toLowerCase()
+    );
+    if (isExisting) {
+      alert(`${isExisting.name} is already in contacts`);
+      return;
+    } else {
+      const contactToAdd = {
+        name: name,
+        number: Number(number),
+        id: nanoid(),
+      };
+
+      localStorage.setItem(
+        'contacts',
+        JSON.stringify([...contacts, contactToAdd])
+      );
+      setContacts([...contacts, contactToAdd]);
+    }
+  };
+
+  const removeContact = id => {
+    const updatedContacts = contacts.filter(contact => contact.id !== id);
+    localStorage.setItem('contacts', JSON.stringify(updatedContacts));
+    setContacts(updatedContacts);
+  };
+
+  const onFilterInput = event => {
+    setFilter(event.target.value);
+  };
+
+  const getFilteredContacts = () => {
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className={styles.wrapper}>
+      <h1>Phonebook</h1>
+      <ContactForm addContact={addContact} />
+      <h2>Contacts</h2>
+      <Filter onFilterInput={onFilterInput} />
+      <ContactList
+        contacts={getFilteredContacts()}
+        removeContact={removeContact}
+      />
     </div>
   );
 };
